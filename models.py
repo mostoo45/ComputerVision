035eae793f870ea913e024f0ed5009f4485b64f4
sungmok <mostoo45@naver.com>
2018-06-05 22:39:58 +0900

    minor bug fixed

--- models.py
+++ models.py
@@ -20,8 +20,10 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
-        
+        self.conv1 = nn.Conv2d(1, 32, 5)#(224-5)/1+1=220
+        self.pool = nn.MaxPool2d(2, 2)#32,110
+        self.conv2 = nn.Conv2d(32, 64, 5)#64,106 ->64,53
+        self.fc1 = nn.Linear(64*53*53, 2*68)
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
         
@@ -30,8 +32,11 @@ class Net(nn.Module):
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(x.size(0), -1)
         
-        
+        # one linear layer
+        x = self.fc1(x)
         # a modified x, having gone through all the layers of your model, should be returned
         return x
